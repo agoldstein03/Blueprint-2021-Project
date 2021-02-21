dd0a471348adfb8a0e1fb6631a88d6dd0fd3d436
keshavganapathy <kganapathy23@gmail.com>
2021-02-21 14:15:24 -0500

    partial teacher page done @tytot

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,8 @@
 	import Home from "./Home.svelte";
 	import Student from "./Student.svelte";
 	import Teacher from "./Teacher.svelte";
+	import Track from "./Track.svelte";
+
 	export let url = "";
 </script>
 
@@ -11,29 +13,33 @@
   Router({url})
     main
       Route(path='/')
-        Home
+        Teacher
       Route(path='/submit/:id' let:params="{params}")
         Student(id="{params.id}")
       Route(path='/create/:id' let:params="{params}")
-        Teacher(id="{params.id}")
+        Track
 </template>
 
 <style lang="scss" global>
 	@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap");
+
 	html,
 	body {
 		width: 100%;
 		height: 100%;
 		font-family: "Open Sans", sans-serif;
 	}
+
 	body {
 		margin: 0;
 		box-sizing: border-box;
 	}
+
 	main {
 		text-align: center;
 		margin: 0 auto;
 	}
+
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
--- src/Teacher.svelte
+++ src/Teacher.svelte
@@ -1,57 +1,106 @@
 <script lang="ts">
-	import Track from "./Track.svelte";
+	// import Track from "./Track.svelte";
 	import Peer from "peerjs";
 	import type { PeerDataType } from "./PeerDataType";
 	import { Label } from "attractions";
-	import { onMount } from "svelte";
 
 	export let id: string | undefined;
 
-	let peer: Peer;
+	const peer = new Peer(/*id*/);
+	console.log({ peer });
+	peer.on("open", (id) => {
+		peer.id = peer.id;
+	});
+	peer.on("error", console.error);
 
-	onMount(() => {
-		peer = new Peer(/*id*/);
-		console.log({ peer });
-		peer.on("open", (localId) => {
-			peer.id = peer.id;
+	peer.on("connection", (conn) => {
+		console.log({ conn });
+		conn.on("open", () => {});
+		conn.on("data", (data: PeerDataType) => {
+			console.log({ data });
+			switch (data.type) {
+				case "label":
+					connData[conn.peer].label = data.label;
+					break;
+				case "audio":
+					connData[conn.peer].audio = data.audio;
+					break;
+			}
 		});
-		peer.on("error", console.error);
-
-		peer.on("connection", (conn) => {
-			console.log({ conn });
-			conn.on("open", () => {
-				console.log("OPEN!!");
-				connData[conn.peer] = {};
-			});
-			conn.on("data", (data: PeerDataType) => {
-				console.log({ data });
-				switch (data.type) {
-					case "label":
-						connData[conn.peer].label = data.label;
-						break;
-					case "audio":
-						connData[conn.peer].audio = data.audio;
-						break;
-				}
-			});
-			conn.on("error", console.error);
-		});
+		conn.on("error", console.error);
 	});
 
 	let connData: {
 		[id: string]: {
-			label?: string;
-			audio?: ArrayBuffer;
+			label: string;
+			audio: ArrayBuffer;
 		};
 	} = {};
 </script>
 
 <template lang="pug">
-  p Teacher
-  p {peer?.id}
-  +each("Object.entries(connData) as [id, track] (id)")
-    Track(label="{track.label}" audio="{track.audio}")
+  body
+    div.header
+      h1.title Harmony
+      h2.code a08w9fa
+      h1.thing Joe is Cute
+    div.main
+      
+    p Teacher
+    p {peer.id}
+    +each("Object.entries(connData) as [id, track] (id)")
+      Track(label="{track.label}" audio="{track.audio}")
 </template>
 
 <style lang="scss">
+	.header {
+		width: 100%;
+		border: 10px;
+		border-color: black;
+		background: white;
+		display: flex;
+		text-align: left;
+		margin: auto;
+		align: left;
+	}
+
+	.code {
+		margin: 0 0 0 0;
+		padding: 0 0 0 0;
+		text-align: left;
+		font-weight: 300;
+		font-style: italic;
+		margin: auto;
+	}
+
+	.title {
+		margin-left: 2rem;
+		font-size: 4rem;
+		font-style: italic;
+		background: -webkit-linear-gradient(left, #184cdf, #bc03fc);
+		background-size: 100%;
+		-webkit-background-clip: text;
+		-moz-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		-moz-text-fill-color: transparent;
+		padding: 0;
+	}
+	.nameheader {
+		text-align: left;
+		align: left;
+	}
+
+	.codesection {
+		text-align: center;
+		align: center;
+	}
+
+	html,
+	body {
+		margin: 0 0 0 0;
+		padding: 0 0 0 0;
+		overflow-x: hidden;
+		width: 100%;
+		height: 100%;
+	}
 </style>
